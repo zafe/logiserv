c7bcf30c2fa08fb8f436c30d07bbb1bda970c119
isamed92 <isamed92@gmail.com>
2016-10-29 15:30:50 -0300

    mensaje

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -15,7 +15,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World"); //esto puse yo Isaias
+        
     }
     
 }
